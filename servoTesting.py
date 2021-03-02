bc04a62485a56f3565c5edb652e580fcc77935be
JARudolph01 <JARudolph01@indianatech.net>
2021-03-02 20:01:35 +0000

    test6

--- servoTesting.py
+++ servoTesting.py
@@ -12,18 +12,37 @@ pi_pwm = GPIO.PWM(ledpin,255)		#create PWM instance with frequency
 pi_pwm2 = GPIO.PWM(ledpin2,255)		#create PWM instance with frequency
 pi_pwm.start(20)				#start PWM of required Duty Cycle 
 pi_pwm2.start(20)				#start PWM of required Duty Cycle 
+#while True:
+#    for duty in range(0,21):
+#        pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+#        pi_pwm2.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+#        sleep(0.01)
+#    
+#    sleep(.3)
+#
+#    for duty in range(0,21):
+#        pi_pwm.ChangeDutyCycle(20-duty)
+#        pi_pwm2.ChangeDutyCycle(20-duty)
+#        sleep(0.01)
+#    
+#
+#    #for duty in range(20,80):
+     #   pi_pwm.ChangeDutyCycle(100-duty) #provide duty cycle in the range 0-100
+      #  pi_pwm2.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100        
+       # sleep(0.01)
+#    
 while True:
     for duty in range(0,21):
         pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
         pi_pwm2.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
-        sleep(0.01)
+        sleep(0.5)
     
     sleep(.3)
 
     for duty in range(0,21):
         pi_pwm.ChangeDutyCycle(20-duty)
         pi_pwm2.ChangeDutyCycle(20-duty)
-        sleep(0.01)
+        sleep(0.5)
     
 
     #for duty in range(20,80):
